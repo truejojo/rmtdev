84e784d5174a4b183db52cd412ec0a846ef2a11f
truejojo <johannesveh@yahoo.de>
2025-06-11 11:45:23 +0200

    Controlled Input of SearchForm

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,11 +1,23 @@
+import { useState } from 'react';
+
 export default function SearchForm() {
+  const [searchText, setSearchText] = useState('');
   return (
-    <form action='#' className='search'>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        setSearchText('');
+      }}
+      action='#'
+      className='search'
+    >
       <button type='submit' title='submit'>
         <i className='fa-solid fa-magnifying-glass'></i>
       </button>
 
       <input
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
         spellCheck='false'
         type='text'
         required
