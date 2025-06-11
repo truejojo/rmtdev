c53902bd902ec16e44012634efb04138abff415e
truejojo <johannesveh@yahoo.de>
2025-06-11 13:17:21 +0200

    Fetch Data in useEffect in SearchForm

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,7 +1,34 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function SearchForm() {
   const [searchText, setSearchText] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  console.log(searchResults);
+
+  useEffect(() => {
+    if (!searchText) return;
+
+    const fetchRequest = async () => {
+      try {
+        const response = await fetch(
+          `https://bytegrad.com/course-assets/projects/rmtdev/api/data?search=${searchText}`,
+        );
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
+
+        const data = await response.json();
+        setSearchResults(data.jobItems);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchRequest();
+  }, [searchText]);
+
   return (
     <form
       onSubmit={(e) => {
