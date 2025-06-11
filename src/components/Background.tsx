3aac3c0e18c9b69128f6f59711f903bc0ce83d2d
truejojo <johannesveh@yahoo.de>
2025-06-11 11:28:34 +0200

    Update Background component

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,10 +1,3 @@
 export default function Background() {
-  return (
-    <div className="background">
-      <img
-        src="https://bytegrad.com/course-assets/images/rn-image-2.png"
-        alt="Background pattern"
-      />
-    </div>
-  );
+  return <div className='background'></div>;
 }
