7d1c27e2c933eac3b7e84c5e7a8b4c75b4f3c980
truejojo <johannesveh@yahoo.de>
2025-06-12 10:07:29 +0200

    Add hook: SearchResult

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Background from './Background';
 import Container from './Container';
 import Footer from './Footer';
@@ -14,37 +14,11 @@ import JobList from './JobList';
 import PaginationControls from './PaginationControls';
 import ResultsCount from './ResultsCount';
 import SortingControls from './SortingControls';
+import { useSearchResults } from '../hooks/SearchResults';
 
 function App() {
   const [searchText, setSearchText] = useState('');
-  const [searchResults, setSearchResults] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    if (!searchText) return;
-
-    const fetchRequest = async () => {
-      setIsLoading(true);
-      try {
-        const response = await fetch(
-          `https://bytegrad.com/course-assets/projects/rmtdev/api/data?search=${searchText}`,
-        );
-
-        if (!response.ok) {
-          throw new Error('Failed to fetch data');
-        }
-
-        const data = await response.json();
-        setSearchResults(data.jobItems);
-      } catch (error) {
-        console.error(error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    fetchRequest();
-  }, [searchText]);
+  const { searchResults, isLoading } = useSearchResults(searchText);
 
   return (
     <>
---
+++ src/hooks/SearchResults.ts
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react';
+
+export const useSearchResults = (searchText: string) => {
+  const [searchResults, setSearchResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (!searchText) return;
+
+    const fetchRequest = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch(
+          `https://bytegrad.com/course-assets/projects/rmtdev/api/data?search=${searchText}`,
+        );
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
+
+        const data = await response.json();
+        setSearchResults(data.jobItems);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchRequest();
+  }, [searchText]);
+
+  return { searchResults, isLoading };
+};
