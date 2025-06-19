33d99ee740ddef3a0bbfba9a9d4ae7b1a8973034
truejojo <johannesveh@yahoo.de>
2025-06-19 16:45:27 +0200

    add close popover functionality with useRef

--- src/components/BookmarksButton.tsx
+++ src/components/BookmarksButton.tsx
@@ -1,13 +1,34 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { TriangleDownIcon } from '@radix-ui/react-icons';
 import BookmarksPopover from './BookmarksPopover';
 
 export default function BookmarksButton() {
   const [isOpen, setIsOpen] = useState(false);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const popoverRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClick = (e: MouseEvent) => {
+      // Schließen, wenn Klick außerhalb von Button und Popover
+      if (
+        !buttonRef.current?.contains(e.target as Node) &&
+        !popoverRef.current?.contains(e.target as Node)
+      ) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener('click', handleClick);
+
+    return () => {
+      document.removeEventListener('click', handleClick);
+    };
+  }, []);
 
   return (
     <section>
       <button
+        ref={buttonRef}
         onClick={() => setIsOpen((prevIsOpen) => !prevIsOpen)}
         type='button'
         className='bookmarks-btn'
@@ -15,7 +36,7 @@ export default function BookmarksButton() {
         Bookmarks <TriangleDownIcon />
       </button>
 
-      {isOpen && <BookmarksPopover />}
+      {isOpen && <BookmarksPopover ref={popoverRef} />}
     </section>
   );
 }
--- src/components/BookmarksPopover.tsx
+++ src/components/BookmarksPopover.tsx
@@ -1,19 +1,20 @@
+import { forwardRef } from 'react';
 import Spinner from './Spinner';
 import { useBookmarkContext } from '../hooks/useBookmarkContext';
 import { useJobItems } from '../hooks/useJobItems';
 import JobList from './JobList';
 
-const BookmarksPopover = () => {
+const BookmarksPopover = forwardRef<HTMLDivElement>((_, ref) => {
   const { bookmarks } = useBookmarkContext();
   const { jobItmes, isLoading } = useJobItems(bookmarks);
 
   return (
-    <div className='bookmarks-popover'>
+    <div ref={ref} className='bookmarks-popover'>
       {isLoading && <Spinner />}
 
       {!isLoading && <JobList searchResults={jobItmes} isLoading={false} />}
     </div>
   );
-};
+});
 
 export default BookmarksPopover;
